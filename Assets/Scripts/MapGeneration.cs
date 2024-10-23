e191bd79bba7078e4347ddc85c7c48e290348568
FeariDev <140981551+FeariDev@users.noreply.github.com>
2024-10-23 14:54:00 +0300

    Mapgen fix and done
    +Fixed mapgen bug
    +Added last ground chunk spawning
    +Prefabbed important stuff

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -8,18 +8,18 @@ public class MapGeneration : MonoBehaviour
     [SerializeField] int numberOfChunks = 4;
 
     [Header("Prefabs")]
+    [SerializeField] GameObject lastGroundChunk;
     [SerializeField] List<GameObject> groundChunks = new List<GameObject>();
     [SerializeField] List<GameObject> housePrefab = new List<GameObject>();
     [SerializeField] Transform groundChunkStartPos;
-    private Transform nextChunkPos;
-    private Transform housePresetPos;
+    public Transform nextChunkPos;
     
     void Start() {
         MapInstantiate();
     }
 
     void MapInstantiate() {
-        for(int i = numberOfChunks; i > 0; i--) {
+        for(int i = numberOfChunks; i >= 0; i--) {
             if(i == numberOfChunks) {
                 GameObject newChunk = Instantiate(
                     groundChunks[0], 
@@ -33,17 +33,19 @@ public class MapGeneration : MonoBehaviour
                     HouseInstantiate(groundChunk.housePos[j]);
                 }
             }
+            else if(i == 0) {
+                Instantiate(lastGroundChunk, nextChunkPos.position, lastGroundChunk.transform.rotation);
+            }
             else {
                 int chunkNumber = Random.Range(0, groundChunks.Count);
 
                 GameObject newChunk = Instantiate(
                     groundChunks[chunkNumber], 
-                    groundChunkStartPos.position, 
-                    groundChunks[0].transform.rotation);
+                    nextChunkPos.position, 
+                    groundChunks[chunkNumber].transform.rotation);
                 GroundChunk groundChunk = newChunk.GetComponent<GroundChunk>();
 
                 nextChunkPos = groundChunk.nextChunkPos;
-                newChunk.transform.position = groundChunk.nextChunkPos.position;
 
                 for(int j = groundChunk.housePos.Count - 1; j >= 0; j--) {
                     HouseInstantiate(groundChunk.housePos[j]);
