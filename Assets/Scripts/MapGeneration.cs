268f616905a310eec09269a81ddea851805757bf
FeariDev <s2300814@edu.bc.fi>
2024-10-23 12:11:10 +0300

    First
    +Mapgen

---
+++ Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour
+{
+    [SerializeField] int numberOfChunks = 4;
+    
+    void Start() {
+        
+    }
+}
